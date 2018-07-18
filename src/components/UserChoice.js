083dfcddb794a5b029c4c4510cc9ecfef496e1f5
Alize Nguyen <alizenguyen@gmail.com>
2018-07-18 17:14:01 -0500

    dropdown menu added and css file for userpage

--- src/components/UserChoice.js
+++ src/components/UserChoice.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { loadingUsers } from '../actions/users'
-import { WSAEUSERS } from 'constants';
+import '../css/UserChoice.css';
 
 class UserChoice extends Component {
   state = {
     users: {}
   }
 
+  //CALLS getUsers() FUNCTION AND SETS THE STATE AS THE RESPONSE
   componentDidMount() {
     this.props.getUsers().then(response => {
       this.setState({ users: response.users})
@@ -19,25 +20,31 @@ class UserChoice extends Component {
     
     return (
       <div>
-        <h1>Choose User:</h1>
-        <ul>
-          {Object.keys(users).map( user => (
-            <li key={users[user].id}>
-              {users[user].name}
-            </li>  
-          ))}
-        </ul>
+        <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Pick A User
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            {Object.keys(users).map(user => (
+              <li className="user-dropdown" key={users[user].id}>
+                <a> {users[user].name} </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     )
   }
 }
 
+//GRABS THE USERS AS A PROP
 const mapStateToProps = state => {
   return {
     users: state.users
   }
 }
 
+//GRABS THE loadingUser() FUNCTION FROM ACTIONS TO BE ABLE TO USE IN THIS COMPONENT
 const mapDispatchToProps = dispatch => {
   return {
     getUsers: () => dispatch(loadingUsers())
