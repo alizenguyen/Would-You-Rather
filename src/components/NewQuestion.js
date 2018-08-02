0b8583ec5a22f6722432fa4413d3360886c52683
Alize Nguyen <alizenguyen@gmail.com>
2018-08-02 16:06:26 -0500

    new question component put together and complete

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,24 +1,24 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import Nav from './Nav'
+import '../css/NewQuestion.css'
 
 class NewQuestion extends Component {
 
   render() {
     const { authedUserAvatar } = this.props
-    
+
     return(
       <Fragment>
         <Nav avatar={authedUserAvatar}/>
         <div className="new-question-full-div">
-          <h2>Create New Question</h2>
+          <h2 className='complete-question-title'>Create New Question</h2>
           <form className='new-question-body'>
-            <p> Complete the question: </p>
-            <h3> Would you rather... </h3>
-            <input placeholder='Enter Option One Here'/>
-            <h3> OR </h3>
-            <input placeholder='Enter Option Two Here'/>
-            <input type='submit' />
+            <h1 className='title'> Would you rather... </h1>
+            <input placeholder='Enter Option One Here' className='question-option'/>
+            <h3 className='complete-question-title'> OR </h3>
+            <input placeholder='Enter Option Two Here' className='question-option'/>
+            <input type='submit' className='submit-btn'/>
           </form>
         </div>
       </Fragment>
