d82eed41780a2fb786f19945fda03b35eabcffe0
Alize Nguyen <alizenguyen@gmail.com>
2018-08-02 21:40:35 -0500

    file cleanup

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,7 +1,6 @@
 import { getInitialData } from '../utils/api'
 import { loadingUsers } from '../actions/users'
 import { loadingQuestions } from '../actions/questions'
-import { saveAuthedUser } from '../actions/authedUser'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
 //GETS handInitialData FROM API AND APPLIES IT TO ACTION CREATORS
@@ -9,10 +8,9 @@ export function handleInitialData () {
   return (dispatch) => {
     dispatch(showLoading())
     return getInitialData()
-      .then(({ users, questions, selectUser })=>{
+      .then(({ users, questions })=>{
         dispatch(loadingUsers(users))
         dispatch(loadingQuestions(questions))
-        //dispatch(saveAuthedUser(selectUser))
         dispatch(hideLoading())
       })
   }
